c22371e9cf35da3bafee5a8f02586f1016717f14
Pythia <qsmith@pythia.cf>
2022-09-30 10:04:13 -0500

    Create ProjectInformation.cpp

---
+++ not_added_code/ProjectInformation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// Variables (Defines and Strs)
+std::string DevelopmentCodename = "Springfield";
+std::string ProgramStartDevelop = "October 3rd, 2022.";
+std::string ProgramVersionNumber = "0.0.1";
+std::bool ProgramStillInDevelop = true;
+
+// main() is where program execution begins.
+int main() {
+     std::cout("Development Codename:" << DevelopmentCodename << endl;
+	 std::cout << "The development started on:" << ProgramStartDevelop << endl;
+	 if (ProgramStillInDevelop === true) {
+		 std::cout << "The Program is Still in Development, get working";
+	 } else {
+	 	std::cout << "The program is no longer in development stages, it is in production.";
+	 }
+	std
+     return 0;
+}
