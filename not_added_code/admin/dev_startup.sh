e7c6af910463999cf7ff6690cffb52d76bbd5e9b
Pythia <qsmith@pythia.cf>
2022-09-30 22:29:19 -0500

    Update dev_startup.sh

--- not_added_code/admin/dev_startup.sh
+++ not_added_code/admin/dev_startup.sh
@@ -13,7 +13,6 @@ rm -rf /home/root/.cache/thumbnails/*
 find . -name ".DS_Store" -delete
 
 
-
 clear
-echo -e "👋 Welcome to the Pythia Development Environment!\n \n ✍ Write code in the integrated VSCode area, or use a terminal text editor like Nano, or VIM.\n \n 💤 When you're ready to push some commits to GitHub, just type ghs (git add . -v && git commit -m [commit message] -S && git push). \n \n"
+echo -e "👋 Welcome to the Pythia Development Environment!\n \n ✍ Write code in the integrated VSCode area, or use a terminal text editor like Nano, or VIM.\n \n 💤 When you're ready to push some commits to GitHub, just type ghs (git add . -v && git commit -m [commit message] -S && git push). \n \n Time and Date: $date" 
 exit 0
