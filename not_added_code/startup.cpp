4b0a29546266f41a9c8f8ecfd414b8d8c0d358c7
Pythia <qsmith@pythia.cf>
2022-10-01 15:38:04 -0500

    Update startup.cpp

--- not_added_code/startup.cpp
+++ not_added_code/startup.cpp
@@ -2,5 +2,12 @@
 #include <chrono>
 
 int main() {
-  std::cout << "👋 Oh! Didn't see you there! Welcome to the terminal for the Pythia Development Environment!" << endl;
   
+  // Variabels
+  std::string PythiaDevEnvironment = "bash";
+  
+  std::cout << "👋 Oh! Didn't see you there! Welcome to the terminal for the Pythia Development Environment!" << endl;
+  std::cout << "The Command Interpreter is" << PythiaDevEnvironment << endl;
+  std::cout << "" << endl;
+
+}
