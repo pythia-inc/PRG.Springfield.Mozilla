a821c5578ae848baaee4149341983520a42494e4
Pythia <qsmith@pythia.cf>
2022-10-06 14:17:37 +0000

    Reset... again.

--- browser/installer/windows/stub.tag
+++
@@ -1,4 +0,0 @@
-;!@Install@!UTF-8!
-Title="Pythia Springfield"
-RunProgram="setup-stub.exe"
-;!@InstallEnd@!
--- js/src/jit-test/tests/bug1787730.js
+++
@@ -1,3 +0,0 @@
-// |jit-test| --delazification-mode=concurrent-df+on-demand; skip-if: !('oomTest' in this)
-
-oomTest(() => evalcx(0));
